3d0fe9c01c4dad3dcdcc1d0bf665f275b487e018
Dario Berzano <dario.berzano@cern.ch>
2018-11-15 23:14:49 +0100

    Improve CI and deployment of Markdown pages
    * Filter out Markdown lines with badges for PyPi
    * Test changes in `setup.py`

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -ex
 cd "$(dirname "$0")"/..
 
+# What tests to run based on what changed (on Travis)
+if [[ $TRAVIS_COMMIT_RANGE ]]; then
+  RUN_SETUP=1
+  git diff --name-only $TRAVIS_COMMIT_RANGE | grep -q ^setup.py$ || RUN_SETUP=
+fi
+
 # Pylint
 find . -name '*.py' -a -not -name 'setup.py' | xargs pylint
 
-# Deploy if this is a tag (do it only for one Python version, not all matrix tests)
+# Deployment/test sdist creation
 if [[ $TRAVIS_PYTHON_VERSION == 3* && $TRAVIS_TAG && $TRAVIS_PULL_REQUEST == false ]]; then
+  # Deploy if this is a tag (do it only for one Python version, not all matrix tests)
   sed -i.deleteme -e "s/LAST_TAG/${TRAVIS_TAG}/g" setup.py
   rm -f *.deleteme
   git clean -fxd
@@ -20,4 +27,7 @@ password = $PYPI_PASSWORD
 EOF
   twine upload dist/*
   rm -f ~/.pypirc
+elif [[ $RUN_SETUP ]]; then
+  # Do not deploy, but setup scripts changed: try to create sdist
+  python setup.py sdist
 fi
