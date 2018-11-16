62e780f8568832f8e8468396fa12d12ae1635bd1
Matteo Concas <mconcas@cern.ch>
2018-11-16 02:51:34 +0100

    Add possibility to override inner configuration

--- alidock/__init__.py
+++ alidock/__init__.py
@@ -9,6 +9,8 @@ import os
 import os.path
 import json
 import subprocess
+import yaml
+from yaml import YAMLError
 import docker
 import jinja2
 import requests
@@ -31,34 +33,48 @@ class AliDock(object):
 
     def __init__(self):
         self.cli = docker.from_env()
-        self.dockName = "/alidock"
-        self.imageName = "alisw/alidock:latest"
-        self.dirOutside = os.path.expanduser("~/alidock")
-        self.dirInside = "/home/alidock"
-        self.logRelative = ".init.log"
         self.lastUpdRelative = ".alidock_last_updated"
         self.updatePeriod = 43200  # 12h
+        self.conf = {
+            "dockName"    : "/alidock",
+            "imageName"    : "alisw/alidock:latest",
+            "dirOutside"  : os.path.expanduser("~/alidock"),
+            "dirInside"   : "/home/alidock",
+            "logRelative" : ".init.log"
+        }
+        self.parseConfig()
+
+    def parseConfig(self):
+        confFile = self.conf["dirOutside"] + "/.alidock-config.yaml"
+        if os.path.isfile(confFile):
+            try:
+                confOverride = yaml.safe_load(open(confFile).read())
+                for k in self.conf:
+                    self.conf[k] = confOverride.get(k, self.conf[k])
+            except (IOError, YAMLError, AttributeError):
+                LOG.error("Configuration file {0} either unreadable or malformed.".format(confFile))
 
     def isRunning(self):
         try:
-            self.cli.containers.get(self.dockName)
+            self.cli.containers.get(self.conf["dockName"])
         except docker.errors.NotFound:
             return False
         return True
 
     def getSshCommand(self):
         try:
-            attrs = self.cli.containers.get(self.dockName).attrs
+            attrs = self.cli.containers.get(self.conf["dockName"]).attrs
             sshPort = attrs["NetworkSettings"]["Ports"]["22/tcp"][0]["HostPort"]
         except (docker.errors.NotFound, KeyError) as exc:
             raise AliDockError("cannot find container, maybe it did not start up properly: "
                                "check log file {logOutside} for details. Error: {msg}"
-                               .format(logOutside=os.path.join(self.dirOutside, self.logRelative),
+                               .format(logOutside=os.path.join(self.conf["dirOutside"],
+                                                               self.conf["logRelative"]),
                                        msg=exc))
         return ["ssh", "localhost", "-p", str(sshPort), "-Y", "-F/dev/null",
                 "-oForwardX11Trusted=no", "-oUserKnownHostsFile=/dev/null",
                 "-oStrictHostKeyChecking=no", "-oLogLevel=QUIET",
-                "-i", os.path.join(self.dirOutside, ".ssh", "id_rsa")]
+                "-i", os.path.join(self.conf["dirOutside"], ".ssh", "id_rsa")]
 
     def waitSshUp(self):
         for _ in range(0, 40):
@@ -76,45 +92,45 @@ class AliDock(object):
         os.execvp("ssh", self.getSshCommand() + (cmd if cmd else []))
 
     def rootShell(self):
-        os.execvp("docker", ["docker", "exec", "-it", self.dockName, "/bin/bash"])
+        os.execvp("docker", ["docker", "exec", "-it", self.conf["dockName"], "/bin/bash"])
 
     def run(self):
         # Create directory to be shared with the container
         try:
-            os.mkdir(self.dirOutside)
+            os.mkdir(self.conf["dirOutside"])
         except OSError as exc:
-            if not os.path.isdir(self.dirOutside) or exc.errno != errno.EEXIST:
+            if not os.path.isdir(self.conf["dirOutside"]) or exc.errno != errno.EEXIST:
                 raise AliDockError("cannot create directory {dir} to share with container, "
-                                   "check permissions".format(dir=self.dirOutside))
+                                   "check permissions".format(dir=self.conf["dirOutside"]))
 
         # Create initialisation script
         initSh = jinja2.Template(resource_string("alidock.helpers", "init.sh.j2"))
         userId = os.getuid()
         userName = getpwuid(userId).pw_name
-        initShPath = os.path.join(self.dirOutside, ".init.sh")
+        initShPath = os.path.join(self.conf["dirOutside"], ".init.sh")
         with open(initShPath, "w") as fil:
-            fil.write(initSh.render(sharedDir=self.dirInside,
-                                    logRelative=self.logRelative,
+            fil.write(initSh.render(sharedDir=self.conf["dirInside"],
+                                    logRelative=self.conf["logRelative"],
                                     userName=userName,
                                     userId=userId))
         os.chmod(initShPath, 0o755)
 
         # Start container with that script
-        self.cli.containers.run(self.imageName,
-                                command=[os.path.join(self.dirInside, ".init.sh")],
+        self.cli.containers.run(self.conf["imageName"],
+                                command=[os.path.join(self.conf["dirInside"], ".init.sh")],
                                 detach=True,
                                 auto_remove=True,
                                 cap_add=["SYS_PTRACE"],
-                                name=self.dockName,
-                                mounts=[docker.types.Mount(self.dirInside,
-                                                           self.dirOutside, type="bind")],
+                                name=self.conf["dockName"],
+                                mounts=[docker.types.Mount(self.conf["dirInside"],
+                                                           self.conf["dirOutside"], type="bind")],
                                 ports={"22/tcp": None})  # None == random port
 
         return True
 
     def stop(self):
         try:
-            self.cli.containers.get(self.dockName).remove(force=True)
+            self.cli.containers.get(self.conf["dockName"]).remove(force=True)
         except docker.errors.NotFound:
             pass  # final state is fine, container is gone
 
