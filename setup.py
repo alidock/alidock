8e6dfbccc879fd92f6da7359ebd33fae1d25546e
Dario Berzano <dario.berzano@gmail.com>
2018-11-21 10:51:16 +0100

    Add host init and Time Machine/Spotlight exclusion (#30)

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
   # installed, specify them here. Note that you need to specify those files in
   # MANIFEST.in as well, since Python tools behave inconsistently
   include_package_data=True,
-  package_data={ "alidock.helpers": ["init.sh.j2"] },
+  package_data={ "alidock.helpers": ["init-inside.sh.j2", "init-outside.sh.j2"] },
 
   # Although 'package_data' is the preferred approach, in some case you may
   # need to place data files outside of your packages. See:
