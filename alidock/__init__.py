51212a0cc31038f8543b7998369b24b866f5f999
Dario Berzano <dario.berzano@cern.ch>
2018-11-29 22:58:35 +0100

    WIP2

--- alidock/__init__.py
+++ alidock/__init__.py
@@ -245,8 +245,7 @@ class AliDock(object):
             LOG.warning("Updating alidock automatically")
             updateEnv = os.environ
             updateEnv["ALIDOCK_ARGS"] = " ".join(sys.argv[1:])
-            print(updateEnv)
-            exit(2)
+            updateEnv["ALIDOCK_RUN"] = "1"
             os.execvpe("bash",
                       ["bash", "-c",
                        "bash <(curl -fsSL {url}) --no-check-docker --quiet".format(url=updateUrl)],
@@ -256,7 +255,7 @@ class AliDock(object):
         """Check for client updates (alidock) without performing them. Returns True if updates are
            found, false otherwise."""
 
-        return True  # TODO
+        #return True  # TODO
 
         if str(require(__package__)[0].version) == "LAST-TAG":
             # No check for local development or version from VCS
