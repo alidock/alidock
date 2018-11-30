f572e1003fc8dbbcab911f483ed9b8804a20698a
Sara Vallero <svallero@to.infn.it>
2018-11-30 11:48:41 +0100

    Fixed line too long

--- alidock/__init__.py
+++ alidock/__init__.py
@@ -86,7 +86,8 @@ class AliDock(object):
         return ["ssh", "localhost", "-p", str(sshPort), "-Y", "-F/dev/null",
                 "-oForwardX11Trusted=no", "-oUserKnownHostsFile=/dev/null", "-oLogLevel=QUIET",
                 "-oStrictHostKeyChecking=no", "-oForwardX11Timeout=596h",
-                "-i", os.path.join(os.path.expanduser(self.conf["dirOutside"]), ".alidock-ssh", "alidock.pem")]
+                "-i", os.path.join(os.path.expanduser(self.conf["dirOutside"]),
+                ".alidock-ssh", "alidock.pem")]
 
     def waitSshUp(self):
         for _ in range(0, 50):
