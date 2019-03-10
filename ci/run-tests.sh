40fdf63f3301d9224c9d52912cb196fd5a7ef9d9
Dario Berzano <dario.berzano@cern.ch>
2019-03-10 17:32:39 +0100

    Improve pip deployment
    * Produce wheels (PEP-491) instead of sdist tarballs for Python 2 and 3
    * Always run `twine check`
    * Deploy PRs to test repository

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -17,24 +17,36 @@ type pylint
 find . -name '*.py' -a -not -path './dist/*'  \
                     -a -not -path './build/*' | xargs pylint
 
-# Deployment/test sdist creation
-if [[ $TRAVIS_PYTHON_VERSION == 3* && $TRAVIS_TAG && $TRAVIS_PULL_REQUEST == false ]]; then
-  # Deploy if this is a tag (do it only for one Python version, not all matrix tests)
-  sed -i.deleteme -e "s/LAST-TAG/${TRAVIS_TAG}/g" setup.py
-  rm -f *.deleteme
+# Produce wheel and deploy it
+if [[ $TRAVIS_TAG && $TRAVIS_PULL_REQUEST == false ]]; then
+  # Real deployment: use official index server
+  PIP_REPO=
+  PIP_PASS=$PYPI_PASSWORD  # encrypted from Travis CI
+  PIP_USER=dberzano
+  PIP_TAG=$TRAVIS_TAG
+elif [[ $TRAVIS_PULL_REQUEST ]]; then
+  # Test deployment: use test index server
+  PIP_REPO="https://test.pypi.org/legacy/"
+  PIP_PASS=$PYPI_TEST_PASSWORD  # encrypted from Travis CI
+  PIP_USER=dberzano
+  PIP_TAG="0.0.post${TRAVIS_PULL_REQUEST}.dev${TRAVIS_BUILD_NUMBER}"
+fi
+
+if [[ $PIP_USER ]]; then
+  sed -i.deleteme -e "s/LAST-TAG/${PIP_TAG}/g" setup.py
+  rm -f setup.py.deleteme
   git clean -fxd
   git diff
-  python setup.py sdist
-  cat > ~/.pypirc <<EOF
-[distutils]
-index-servers=pypi
-[pypi]
-username = dberzano
-password = $PYPI_PASSWORD
-EOF
-  twine upload dist/*
-  rm -f ~/.pypirc
-elif [[ $RUN_SETUP ]]; then
-  # Do not deploy, but setup scripts changed: try to create sdist
-  python setup.py sdist
+fi
+
+python setup.py bdist_wheel
+twine check dist/*
+if [[ $PIP_USER ]]; then
+  twine upload ${PIP_REPO:+--repository-url "$PIP_REPO"} \
+               --skip-existing \
+               -u "$PIP_USER" \
+               -p "$PIP_PASS" \
+               dist/*
+else
+  echo "Not uploading: test not on Travis CI"
 fi
