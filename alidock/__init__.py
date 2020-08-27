8e388a3f1b8d7b4289cf348e58ec3ea34cdd4d86
Matteo Concas <mconcas@cern.ch>
2020-08-27 14:09:40 +0200

    Revert "Circumvent possible Python2 bug (#148)"
    This reverts commit 1761b0d745a98ed32e065624f44abb5c47e7ee1b.

--- alidock/__init__.py
+++ alidock/__init__.py
@@ -349,7 +349,7 @@ class AliDock(object):
                     if not os.path.isdir(tsDir) or exc.errno != errno.EEXIST:
                         raise exc
                 with open(tsFn, "w") as fil:
-                    fil.write(str(now).decode('utf-8'))
+                    fil.write(str(now))
 
             if caught is not None:
                 # pylint: disable=raising-bad-type
